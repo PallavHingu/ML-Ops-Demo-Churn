65a0942608d9eddc3c5a200e568899836c38d3d8
PallavHingu <pallav.hingu@publicissapient.com>
2025-04-28 00:41:43 +0100

    printed more temp

--- src/mlops/model/trainer.py
+++ src/mlops/model/trainer.py
@@ -6,10 +6,10 @@ import numpy as np, tempfile
 class ModelTrainer(BaseComponent):
     """Trains the model using input data. Trains a Gradient Boost Classifier model."""
     def run(self, X: np.ndarray, y: np.ndarray):
-        self.log.info("Training GradientBoostingClassifier…")
-        self.log.info("...")
+        self.log.info("Training Gradient Boosting Classifier …")
+        self.log.info("..mm.")
         clf = GradientBoostingClassifier(random_state=42)
-        self.log.info("..")
+        self.log.info(".n.")
         self.log.info(str(type(X)))
         self.log.info(str(type(y)))
         clf.fit(X, y)
